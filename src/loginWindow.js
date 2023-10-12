ffdc0de0cab986df258de98fd95ba53d5b0190c6
Vowxky <114261590+vowxky@users.noreply.github.com>
2023-10-11 22:34:47 -0500

    Login added

--- src/loginWindow.js
+++ src/loginWindow.js
@@ -1,1 +1,169 @@
+const {app, BrowserWindow} = require('electron');
+const pkg = require('../package.json');
+const path = require('path');
+const fs = require('fs');
+const { ipcMain } = require('electron')
+const DiscordRPC = require('discord-rpc');
+const clientId = '1161452615943589929';
+let appdataPath = '';
+const RPC = new DiscordRPC.Client({ transport: 'ipc'});
+let discordTime = Date.now();
+DiscordRPC.register(clientId);
 
+let secondaryWindow = null;
+let mainWindow = null;
+
+let pathFolderName = pkg['path'];
+
+
+function createFolder() {
+  if (!fs.existsSync(appdataPath)) {
+    fs.mkdirSync(appdataPath);
+  }
+}
+
+async function setActivity() {
+    if (!RPC) return;
+    RPC.setActivity({
+        details: `Esperando en el Login`,
+        startTimestamp: discordTime,
+        largeImageKey: 'vuxilaunch',
+        largeImageText: `vuxilaunch`,
+        instance: false,
+        buttons: [
+            {
+                label: `Discord`,
+                url: `https://discord.gg/XjPVtkGu`,
+            },
+            {
+                label: `Source Code`,
+                url: `https://github.com/vowxky/VuxiLaunch`,
+            }
+        ]
+    });
+ };
+
+RPC.on('ready', async () => {
+    setActivity();
+ 
+    setInterval(() => {
+        setActivity();
+    }, 86400 * 1000);
+ });
+ RPC.login({ clientId }).catch(err => console.error(err));
+ 
+
+ function createWindow(options) {
+  const windowauth = {
+    titleBarStyle: 'hidden',
+    icon : "./src/assets/icon/icon.png",
+    resizable: false,
+    maximizable: false,
+    width: 450,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    ...options,
+  };
+
+  if (options.secondary) {
+      secondaryWindow = new BrowserWindow(windowauth);
+      secondaryWindow.loadFile(options.url);
+
+      secondaryWindow.on('closed', () => {
+        secondaryWindow = null;
+      });
+  } else {
+      mainWindow = new BrowserWindow(windowauth);
+      mainWindow.loadFile(options.url);
+
+      mainWindow.on('closed', () => {
+        mainWindow = null;
+      });
+  }
+}
+  app.whenReady().then(() => {
+
+    appdataPath = path.join(process.env.APPDATA, `.${pathFolderName}`);
+
+    createFolder(appdataPath);
+    
+    setTimeout(() => {
+      createWindow({ url: './src/panels/html/login.html' });
+    }, 2000);
+  
+    app.on('activate', () => {
+      if (mainWindow === null) {
+        createWindow({ url: './src/panels/html/login.html' });
+      }
+    });
+  });
+  
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+
+  ipcMain.on('save-account', (event, account) => {
+    windowauth.account = account;
+  });
+
+
+  ipcMain.on('solicitar-cuenta', (event) => {
+    event.sender.send('responder-cuenta', windowauth.account);
+  });
+
+
+  ipcMain.on('set-progress-bar', (event, percentage) => {
+    secondaryWindow.setProgressBar(percentage)
+  });
+
+  ipcMain.on('set-progress-bar-reset', (evt, arg) => {
+    secondaryWindow.setProgressBar(0);
+  });
+
+  ipcMain.on('close', (evt, arg) => {
+    secondaryWindow.close();
+  });
+
+  ipcMain.on('re-open', (evt, arg) => {
+    mainWindow.close();
+    createWindow({
+      url: './src/home.html', 
+      secondary: true,
+      width: 1200, 
+      height: 700, 
+    });
+  });
+
+  ipcMain.on('hide-window', (evt, arg) => {
+    secondaryWindow.hide();
+  });
+
+  ipcMain.on('show-window', (evt, arg) => {
+    secondaryWindow.show();
+  });
+
+  ipcMain.on('change-status-discord', (event, DiscordStatus)=> {
+    RPC.setActivity({
+    details: `${DiscordStatus}`,
+    startTimestamp: discordTime,
+    largeImageKey: 'vuxilaunch',
+    largeImageText: `vuxilaunch`,
+    instance: false,
+    buttons: [
+        {
+            label: `Discord`,
+            url: `https://discord.gg/XjPVtkGu`,
+        },
+        {
+            label: `Source Code`,
+            url: `https://github.com/vowxky/VuxiLaunch`,
+        }
+      ]
+    });
+
+  })
---
+++ src/panels/js/login.js
@@ -0,0 +1,51 @@
+const { ipcRenderer } = require("electron");
+
+class Login{
+    async main(){
+        this.login();
+    }
+    async login(){
+        const input = document.getElementById('nameInput');
+        input.addEventListener('input', (event) => {
+            let inputValue = event.target.value;
+    
+            inputValue = inputValue.replace(/[^\w\s-]/g, '');
+            
+            const maxLength = 16;
+            if (inputValue.length > maxLength) {
+                inputValue = inputValue.substring(0, maxLength);
+            }
+            
+            input.value = inputValue;
+            
+            if (inputValue.length < 3 || inputValue.length > maxLength) {
+                input.style.border = "2px solid red";
+            } else {
+                input.style.border = "1px solid #070707";
+            }
+        });
+        
+
+        document.getElementById('login').addEventListener('click', () => {
+            const inputValue = input.value;
+            const sanitizedValue = inputValue.replace(/\s/g, '');
+
+            if (sanitizedValue.length >= 3 && sanitizedValue.length <= 16) {
+                ipcRenderer.send('re-open');
+                ipcRenderer.send('change-status-discord', 'Esperando en el Menu');
+            } else {
+                input.value = '';
+                input.disabled = true;
+                errorMessage.style.display = "block";
+                errorMessage.innerText = "El nombre de usuario debe tener entre 3 y 16 caracteres.";
+                setTimeout(() => {
+                    input.disabled = false;
+                    errorMessage.style.display = "none";
+                }, 2000);    
+            }
+        });
+    }
+}
+
+const loginMain = new Login();
+loginMain.main();
